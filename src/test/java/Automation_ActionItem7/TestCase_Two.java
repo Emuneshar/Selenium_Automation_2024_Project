58d18c906c6c028b94c51e81c6be403517854f86
Eshwar Muneshar <60401349+Emuneshar@users.noreply.github.com>
2024-12-11 00:01:23 -0500

    Made changes

--- src/test/java/Automation_ActionItem7/TestCase_Two.java
+++ src/test/java/Automation_ActionItem7/TestCase_Two.java
@@ -7,10 +7,13 @@ import org.testng.annotations.Test;
 public class TestCase_Two extends TestParent {
     String url = "https://www.northwesternmutual.com/";
     String xpathFinancialPlanning = "//*[@id = 'nmx-nav-link-primary-financial-planning']";
+    String xpathCollegeSavings = "//*[@id = 'nmx-nav-link-primary-sub-college-savings-plans']";
 
     @Test
-    public void tc001_verifyConnectToAdvisor(){
+    public void tc001_verifyConnectToAdvisor() throws InterruptedException {
         driver.navigate().to(url);
         ReuasableMethodsLoggers.mouseActionHover(driver, xpathFinancialPlanning, logger, "Financial Planning Menu");
+        ReuasableMethodsLoggers.clickMethod(driver, xpathCollegeSavings, logger, "College Savings");
+        Thread.sleep(4000);
     }
 }
