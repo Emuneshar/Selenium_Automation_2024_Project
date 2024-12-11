0d4afaf4346ccecf131fd2a814e19ee064f5cf82
Eshwar Muneshar <60401349+Emuneshar@users.noreply.github.com>
2024-12-10 20:34:36 -0500

    Added code to hover over menu item

--- src/test/java/Automation_ActionItem7/TestCase_Two.java
+++ src/test/java/Automation_ActionItem7/TestCase_Two.java
@@ -1,14 +1,16 @@
 package Automation_ActionItem7;
 
 import Day13_12022024.TestParent;
+import Day14_12032024.ReuasableMethodsLoggers;
 import org.testng.annotations.Test;
 
 public class TestCase_Two extends TestParent {
     String url = "https://www.northwesternmutual.com/";
+    String xpathFinancialPlanning = "//*[@id = 'nmx-nav-link-primary-financial-planning']";
 
     @Test
     public void tc001_verifyConnectToAdvisor(){
         driver.navigate().to(url);
-        
+        ReuasableMethodsLoggers.mouseActionHover(driver, xpathFinancialPlanning, logger, "Financial Planning Menu");
     }
 }
