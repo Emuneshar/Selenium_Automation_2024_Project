6cd85a34d4e8e741fc5d88738d0e2cc3e4d93d72
Eshwar Muneshar <60401349+Emuneshar@users.noreply.github.com>
2024-12-10 20:24:02 -0500

    Added url to test case one

--- src/test/java/Automation_ActionItem7/TestCase_Two.java
+++ src/test/java/Automation_ActionItem7/TestCase_Two.java
@@ -4,8 +4,10 @@ import Day13_12022024.TestParent;
 import org.testng.annotations.Test;
 
 public class TestCase_Two extends TestParent {
+    String url = "https://www.northwesternmutual.com/";
+
     @Test
     public void tc001_(){
-
+        driver.navigate().to(url);
     }
 }
