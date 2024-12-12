ac6302256ed9b30deab0b0f1b0597ff68fe41ee2
Eshwar Muneshar <60401349+Emuneshar@users.noreply.github.com>
2024-12-12 16:30:07 -0500

    Added new code to click on start planning button

--- src/test/java/Automation_ActionItem7/TestCase_Two.java
+++ src/test/java/Automation_ActionItem7/TestCase_Two.java
@@ -8,12 +8,14 @@ public class TestCase_Two extends TestParent {
     String url = "https://www.northwesternmutual.com/";
     String xpathFinancialPlanning = "//*[@id = 'nmx-nav-link-primary-financial-planning']";
     String xpathCollegeSavings = "//*[@id = 'nmx-nav-link-primary-sub-college-savings-plans']";
+    String xpathStartPlanning = "//*[@id = 'college-savings-plans-page-flexible-hero-cta-primary']";
 
     @Test
     public void tc001_verifyConnectToAdvisor() throws InterruptedException {
         driver.navigate().to(url);
         ReuasableMethodsLoggers.mouseActionHover(driver, xpathFinancialPlanning, logger, "Financial Planning Menu");
         ReuasableMethodsLoggers.clickMethod(driver, xpathCollegeSavings, logger, "College Savings");
+        ReuasableMethodsLoggers.clickMethod(driver, xpathStartPlanning, logger, "Start Planning");
         Thread.sleep(4000);
     }
 
