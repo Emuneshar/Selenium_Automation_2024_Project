e14847a04fe41c9c7e835810f6b1ba4068fe011d
Eshwar Muneshar <60401349+Emuneshar@users.noreply.github.com>
2024-12-12 16:52:46 -0500

    Changed user story

--- src/test/java/Automation_ActionItem7/TestCase_Two.java
+++ src/test/java/Automation_ActionItem7/TestCase_Two.java
@@ -6,18 +6,20 @@ import org.testng.annotations.Test;
 
 public class TestCase_Two extends TestParent {
     String url = "https://www.northwesternmutual.com/";
-    String xpathFinancialPlanning = "//*[@id = 'nmx-nav-link-primary-financial-planning']";
-    String xpathCollegeSavings = "//*[@id = 'nmx-nav-link-primary-sub-college-savings-plans']";
+    String xpathFinancialPlanning = "//*[@id = 'nmx-nav-link-primary-investments']";
+    String xpathCollegeSavings = "//*[@id = 'nmx-nav-link-primary-sub-investments-private-wealth-management']";
     String xpathStartPlanning = "//*[@id = 'college-savings-plans-page-flexible-hero-cta-primary']";
-    String xpathSaveForCollege = "//*[@id = 'fafa-interactive-flow-goal-card-save-for-college']";
+    //String xpathSaveFoorCollege = "//*[@id = 'fafa-interactive-flow-goal-card-save-for-college']";
+    String xpathSaveForCollege = "//*[@id='fafa-interactive-flow-goal-card-save-for-college']";
+
+    String xpathNext = "//*[@id = 'in-page-lead-step-1-next-button']";
 
     @Test
-    public void tc001_verifyConnectToAdvisor() throws InterruptedException {
+    public void tc001_verifyPrivateWealthManagementPartners() throws InterruptedException {
         driver.navigate().to(url);
         ReuasableMethodsLoggers.mouseActionHover(driver, xpathFinancialPlanning, logger, "Financial Planning Menu");
         ReuasableMethodsLoggers.clickMethod(driver, xpathCollegeSavings, logger, "College Savings");
-        ReuasableMethodsLoggers.clickMethod(driver, xpathStartPlanning, logger, "Start Planning");
-        ReuasableMethodsLoggers.clickMethod(driver, xpathSaveForCollege, logger, "Save for College");
+        
         Thread.sleep(4000);
     }
 
