3fca2e5e78303d096d16e3f8221de765453924b5
Eshwar Muneshar <60401349+Emuneshar@users.noreply.github.com>
2024-12-13 00:08:01 -0500

    finished code, adding comments

--- src/test/java/Automation_ActionItem7/TestCase_One.java
+++ src/test/java/Automation_ActionItem7/TestCase_One.java
@@ -5,6 +5,8 @@ import Day14_12032024.ReuasableMethodsLoggers;
 import org.testng.annotations.Test;
 
 public class TestCase_One extends TestParent {
+
+    // Strings and xpath storage
     String url = "https://www.northwesternmutual.com/";
     String xpathForMenu = "//*[@id = 'nmx-nav-link-primary-insurance']";
     String xpathForDisabilityCalculator = "//*[@id = 'nmx-nav-link-primary-sub-level2-disability-insurance-calculator']";
@@ -22,33 +24,33 @@ public class TestCase_One extends TestParent {
     String xpathGroupCoverageChoice = "//*[text()='Yes']";
     String xpathEstimate = "//*[@class = 'results-header--di-calc__cov-estimate']";
 
-
+    // Test case 1 with priority 1
     @Test(priority = 1)
     public void tc001_enterAgeAndGender(){
-        driver.navigate().to(url);
-        ReuasableMethodsLoggers.mouseActionHover(driver, xpathForMenu, logger, "Insurance Menu");
-        ReuasableMethodsLoggers.clickMethod(driver, xpathForDisabilityCalculator, logger, "Disability Calculator");
-        ReuasableMethodsLoggers.clickMethod(driver, xpathForCalculateButton, logger, "Calculate Button");
-        ReuasableMethodsLoggers.clickMethod(driver, xpathForAge, logger, "Age Field");
-        ReuasableMethodsLoggers.sendKeysMethod(driver, xpathForAge, "25", logger, "Age Field");
-        ReuasableMethodsLoggers.clickMethod(driver, xpathForGender, logger, "Gender Field");
-        ReuasableMethodsLoggers.clickMethod(driver, xpathSelectGender, logger, "Male");
-        ReuasableMethodsLoggers.clickMethod(driver, xpathNextButton, logger, "Next Button");
+        driver.navigate().to(url); // Navigate to url
+        ReuasableMethodsLoggers.mouseActionHover(driver, xpathForMenu, logger, "Insurance Menu"); // Hover over menu, Insurance
+        ReuasableMethodsLoggers.clickMethod(driver, xpathForDisabilityCalculator, logger, "Disability Calculator"); // click on Disability calculator
+        ReuasableMethodsLoggers.clickMethod(driver, xpathForCalculateButton, logger, "Calculate Button"); // click on calculate button
+        ReuasableMethodsLoggers.clickMethod(driver, xpathForAge, logger, "Age Field"); // click on the age field
+        ReuasableMethodsLoggers.sendKeysMethod(driver, xpathForAge, "25", logger, "Age Field"); // send mock data for age
+        ReuasableMethodsLoggers.clickMethod(driver, xpathForGender, logger, "Gender Field"); // click on the gender field
+        ReuasableMethodsLoggers.clickMethod(driver, xpathSelectGender, logger, "Male"); // select the gender from the dropdown
+        ReuasableMethodsLoggers.clickMethod(driver, xpathNextButton, logger, "Next Button"); // click on next button
+    } // End of test case 1
 
-    }
-
+    // Test case 2 depends on test case 1
     @Test(dependsOnMethods = "tc001_enterAgeAndGender")
     public void tc002_enterIncomeExpensesSavings() {
-        ReuasableMethodsLoggers.clickMethod(driver, xpathIncome, logger, "Income Field");
-        ReuasableMethodsLoggers.sendKeysMethod(driver, xpathIncome, "120000", logger, "Income Field");
-        ReuasableMethodsLoggers.clickMethod(driver, xpathMonthlyExpenses, logger, "Monthly Expenses");
-        ReuasableMethodsLoggers.sendKeysMethod(driver, xpathMonthlyExpenses, "4000", logger, "Monthly Expenses");
-        ReuasableMethodsLoggers.clickMethod(driver, xpathCurrentSavings, logger, "Current Savings");
-        ReuasableMethodsLoggers.sendKeysMethod(driver, xpathCurrentSavings, "400000", logger, "Current Savings");
-        ReuasableMethodsLoggers.clickMethod(driver, xpathNextButton, logger, "Next Button");
+        ReuasableMethodsLoggers.clickMethod(driver, xpathIncome, logger, "Income Field"); // click on income field
+        ReuasableMethodsLoggers.sendKeysMethod(driver, xpathIncome, "120000", logger, "Income Field"); // send mock data to income field
+        ReuasableMethodsLoggers.clickMethod(driver, xpathMonthlyExpenses, logger, "Monthly Expenses"); // click on monthly expense field
+        ReuasableMethodsLoggers.sendKeysMethod(driver, xpathMonthlyExpenses, "4000", logger, "Monthly Expenses"); // send mock data for monthly expenses
+        ReuasableMethodsLoggers.clickMethod(driver, xpathCurrentSavings, logger, "Current Savings"); // click on current savings field
+        ReuasableMethodsLoggers.sendKeysMethod(driver, xpathCurrentSavings, "400000", logger, "Current Savings"); // send mock data for savings
+        ReuasableMethodsLoggers.clickMethod(driver, xpathNextButton, logger, "Next Button"); // click on next button
+    } // End of test case 2
 
-    }
-
+    // Test case 3 depends on test case 2
     @Test(dependsOnMethods = "tc002_enterIncomeExpensesSavings")
     public void tc003_getEstimate() throws InterruptedException {
         ReuasableMethodsLoggers.clickMethodWithIndex(driver, xpathJobDescription, 0, logger, "Description Type");
@@ -58,9 +60,6 @@ public class TestCase_One extends TestParent {
         ReuasableMethodsLoggers.clickMethod(driver, xpathGroupCoverageChoice, logger, "Choice");
         ReuasableMethodsLoggers.clickMethod(driver, xpathNextButton, logger, "Next button");
         String estimate = ReuasableMethodsLoggers.captureText(driver, xpathEstimate, logger, "Price");
-    }
-
-
-
+    } // End of test case 3
 
 }
--- src/test/java/Automation_ActionItem7/TestCase_Two.java
+++ src/test/java/Automation_ActionItem7/TestCase_Two.java
@@ -5,6 +5,8 @@ import Day14_12032024.ReuasableMethodsLoggers;
 import org.testng.annotations.Test;
 
 public class TestCase_Two extends TestParent {
+
+    // Strings and xpath storage
     String url = "https://www.northwesternmutual.com/";
     String xpathInvestments = "//*[@id = 'nmx-nav-link-primary-investments']";
     String xpathWealthManagement = "//*[@id = 'nmx-nav-link-primary-sub-investments-private-wealth-management']";
@@ -15,6 +17,8 @@ public class TestCase_Two extends TestParent {
     String xpathEstatePlanning = "//*[@id = 'nmx-nav-link-primary-sub-estate-planning']";
     String xpathPopupClose = "//*[@id = 'onetrust-accept-btn-handler']";
     String xpathAboutPlanning = "//*[@id = 'hero-description-text-estate-planning-hero']";
+    String xpathBrokerageAccounts = "//*[@id = 'nmx-nav-link-primary-sub-brokerage-accounts-services']";
+    String xpathQuestionAnswer = "//*[@class = 'sc-gKseQn gNzQkc']";
     String expectedResult = "Aspen Park Financial\n" +
             "Blue Column Capital\n" +
             "Cannataro Family Capital Partners\n" +
@@ -43,29 +47,42 @@ public class TestCase_Two extends TestParent {
             "The Atrium Financial Group";
 
     String textToCompare = "Having a plan for your estate—your home, your wealth, your possessions-means you can leave the legacy you want, whether that's to help your family, a charity, or an institution.";
+    String textFromBrokerageAccount = "Questions about brokerage accounts and services? We've got answers.";
 
+
+    // Test Case 1
     @Test
     public void tc001_verifyPrivateWealthManagementPartners() throws InterruptedException {
-        driver.navigate().to(url);
-        ReuasableMethodsLoggers.mouseActionHover(driver, xpathInvestments, logger, "Investments");
-        ReuasableMethodsLoggers.clickMethod(driver, xpathWealthManagement, logger, "Private Wealth Management");
-        Thread.sleep(3000);
-        ReuasableMethodsLoggers.scrollByPixel(driver, 0, 300, logger);
-        ReuasableMethodsLoggers.clickMethod(driver, xpathFindClientGroup, logger, "Find A Private Client Group Advisor");
-        ReuasableMethodsLoggers.selectByText(driver, xpathSelectAState, "New York",logger);
-        String actualResult = ReuasableMethodsLoggers.captureText(driver, xpathResults, logger, "Results");
-        assertEqualsLogger(actualResult, expectedResult);
-    }
+        driver.navigate().to(url); // Navigate to url
+        ReuasableMethodsLoggers.mouseActionHover(driver, xpathInvestments, logger, "Investments"); // Hover over menu item to show hidden menu
+        ReuasableMethodsLoggers.clickMethod(driver, xpathWealthManagement, logger, "Private Wealth Management"); // clicks on item from submenu, Private Wealth Management
+        Thread.sleep(3000); // Sleep so the page can load before scrolling
+        ReuasableMethodsLoggers.scrollByPixel(driver, 0, 300, logger); // Scroll down so the needed element wil be visible
+        ReuasableMethodsLoggers.clickMethod(driver, xpathFindClientGroup, logger, "Find A Private Client Group Advisor"); // click on find an advisor
+        ReuasableMethodsLoggers.selectByText(driver, xpathSelectAState, "New York",logger); // Enters state to drop down menu
+        String actualResult = ReuasableMethodsLoggers.captureText(driver, xpathResults, logger, "Results"); // get slist of partners in that state
+        assertEqualsLogger(actualResult, expectedResult); // checks if the list is matched to what should be there
+    } // End of test case 1
 
+    // Test Case 2
     @Test
-    public void tc002_WhoNeedsAnEstatePlan() throws InterruptedException {
-        driver.navigate().to(url);
-        ReuasableMethodsLoggers.mouseActionHover(driver, xpathFinancialPlanning, logger, "Financial Planning");
-        ReuasableMethodsLoggers.clickMethod(driver, xpathEstatePlanning, logger, "Estate Planning");
-        Thread.sleep(4000);
-        ReuasableMethodsLoggers.clickMethod(driver, xpathPopupClose, logger,"Close pop up");
-        String actualText = ReuasableMethodsLoggers.captureText(driver, xpathAboutPlanning, logger, "Text to check");
-        assertEqualsLogger(actualText, textToCompare);
-    }
+    public void tc002_WhoNeedsAnEstatePlan(){
+        driver.navigate().to(url); // Navigate to url
+        ReuasableMethodsLoggers.mouseActionHover(driver, xpathFinancialPlanning, logger, "Financial Planning"); // Hover over menu item to show hidden menu
+        ReuasableMethodsLoggers.clickMethod(driver, xpathEstatePlanning, logger, "Estate Planning"); // clicks on item from submenu, Estate planning
+        ReuasableMethodsLoggers.clickMethod(driver, xpathPopupClose, logger,"Close pop up"); // closes pop up on the bottom of the page
+        String actualText = ReuasableMethodsLoggers.captureText(driver, xpathAboutPlanning, logger, "Text to check"); // gets the text from the site
+        assertEqualsLogger(actualText, textToCompare); // compares text from the site against what should be there
+    } // End of test case 2
 
-}
+    // Test Case 3
+    @Test
+    public void tc003_VerifyTextBrokerageAccount(){
+        driver.navigate().to(url); // Navigate to url
+        ReuasableMethodsLoggers.mouseActionHover(driver, xpathInvestments, logger, "Investments"); // Hover over menu item to show hidden menu
+        ReuasableMethodsLoggers.clickMethod(driver, xpathBrokerageAccounts, logger, "Brokerage accounts"); // clicks on item from submenu, Brokerage accounts
+        String actualText = ReuasableMethodsLoggers.captureText(driver, xpathQuestionAnswer, logger, "Hero Text"); // gets the text from the site
+        assertEqualsLogger(actualText, textFromBrokerageAccount); // compares text from the site against what should be there
+    } // End of test case 3
+
+} // End of test class
