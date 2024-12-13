41669bf1660cde81e7df85e2f5ff6fb55c7bdd84
Eshwar Muneshar <60401349+Emuneshar@users.noreply.github.com>
2024-12-12 21:19:37 -0500

    Updated Element Name

--- src/test/java/Automation_ActionItem7/TestCase_Two.java
+++ src/test/java/Automation_ActionItem7/TestCase_Two.java
@@ -16,7 +16,7 @@ public class TestCase_Two extends TestParent {
     public void tc001_verifyPrivateWealthManagementPartners() throws InterruptedException {
         driver.navigate().to(url);
         ReuasableMethodsLoggers.mouseActionHover(driver, xpathInvestments, logger, "Investments");
-        ReuasableMethodsLoggers.clickMethod(driver, xpathWealthManagement, logger, "College Savings");
+        ReuasableMethodsLoggers.clickMethod(driver, xpathWealthManagement, logger, "Private Wealth Management");
         Thread.sleep(3000);
         ReuasableMethodsLoggers.scrollByPixel(driver, 0, 300, logger);
         ReuasableMethodsLoggers.clickMethod(driver, xpathFindClientGroup, logger, "Find A Private Client Group Advisor");
